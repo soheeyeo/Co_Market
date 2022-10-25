272448909c57e25b31c431dcd147aa0bbe621ab6
soheeyeo <sohee407@gmail.com>
2022-10-25 23:41:34 +0900

    Add createElement to correct mistake and add img element

--- src/js/components/Header/header.js
+++ src/js/components/Header/header.js
@@ -7,20 +7,26 @@ export default class Header extends Component {
     render() {
         const mainHeader = document.createElement('header');
         mainHeader.setAttribute('class', 'main_header');
-        const headerContainer = document.createElement('header_container');
+        const headerContainer = document.createElement('div');
+        headerContainer.setAttribute('class', 'header_container');
+
+        mainHeader.appendChild(headerContainer);
 
         const logoWrapper = document.createElement('div');
         logoWrapper.setAttribute('class', 'logo_wrapper');
         const logo = document.createElement('a');
         logo.setAttribute('href', '/');
         logo.setAttribute('class', 'logo');
+        const logoImg = document.createElement('div');
+        logoImg.setAttribute('class', 'logo_img');
 
+        logo.appendChild(logoImg);
         logoWrapper.appendChild(logo);
 
         const search = new Search();
         const headerIcons = new HeaderIcons();
 
-        mainHeader.append(logoWrapper, search.render(), headerIcons.render());
+        headerContainer.append(logoWrapper, search.render(), headerIcons.render());
 
         return mainHeader;
     }
