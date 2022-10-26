70e885bc8afcae8c054a1749acc9842412729d3d
soheeyeo <sohee407@gmail.com>
2022-10-26 22:50:57 +0900

    Add Search markup

--- src/js/components/Header/search.js
+++ src/js/components/Header/search.js
@@ -3,16 +3,27 @@ import Component from "../abstractComponent.js";
 export default class Search extends Component {
 
     render() {
-        const searchBox = document.createElement('class', 'search_box');
+        const searchBox = document.createElement('div');
+        searchBox.setAttribute('class', 'search_box');
+        const searchForm = document.createElement('form');
+        searchForm.setAttribute('class', 'search_form');
+        searchForm.method = 'get';
+        searchForm.name = 'search_form';
+
+        searchBox.appendChild(searchForm);
+
         const search = document.createElement('input');
-        search.setAttribute('type', 'text');
-        search.setAttribute('method', 'get');
         search.setAttribute('class', 'search');
+        search.name = 'search';
+        search.type = 'text';
+        search.placeholder = '상품을 검색해보세요!'
 
-        const searchIcon = document.createElement('div');
+        const searchIcon = document.createElement('button');
         searchIcon.setAttribute('class', 'search_icon');
+        searchIcon.type = 'submit';
+        searchIcon.name = 'search_btn';
 
-        searchBox.append(search, searchIcon);
+        searchForm.append(search, searchIcon);
 
         return searchBox;
     }
