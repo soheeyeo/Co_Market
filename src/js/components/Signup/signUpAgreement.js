d8948d2db25e8a5eb5fd3120a2f5e48c8de7dc5d
soheeyeo <sohee407@gmail.com>
2022-11-11 14:57:23 +0900

    Create Agreement component

---
+++ src/js/components/Signup/signUpAgreement.js
@@ -0,0 +1,23 @@
+import Component from "../abstractComponent.js";
+
+export default class Agreement extends Component {
+    
+    render() {
+        const formCheckWrapper = document.createElement('div');
+        formCheckWrapper.setAttribute('class', 'form_check_wrapper');
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.id = 'check_agree';
+        checkbox.required = 'required';
+        const checkAgree = document.createElement('label');
+        checkAgree.setAttribute('class', 'agreement')
+        checkAgree.for = 'check_agree';
+        checkAgree.innerHTML = `
+            <a href="#" class="agreement txt">이용약관</a> 및 <a href="#" class="agreement txt">개인정보처리방침</a>에 대한 내용을 확인하였고 동의합니다.
+        `
+
+        formCheckWrapper.append(checkbox, checkAgree);
+        
+        return formCheckWrapper;
+    }
+}
