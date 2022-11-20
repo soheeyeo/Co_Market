4be1811b9bc28606c2d0d17c3dd103a147f2bf59
soheeyeo <sohee407@gmail.com>
2022-11-20 22:14:32 +0900

    Modify function for tab menu click events

--- src/js/components/ProductTab/productTab.js
+++ src/js/components/ProductTab/productTab.js
@@ -6,32 +6,37 @@ export default class ProductTab extends Component {
         const productTabWrapper = document.createElement('section');
         productTabWrapper.setAttribute('class', 'product_tab_wrapper');
 
-        const detailBtn = document.createElement('button');
-        detailBtn.innerText = '상세정보';
-        detailBtn.classList.add('tab_on');
+        const detailBtnLi = document.createElement('ul');
+        detailBtnLi.setAttribute('class', 'detail_tab_li');
 
-        const reviewBtn = document.createElement('button');
-        reviewBtn.innerText = '리뷰';
-
-        const qaBtn = document.createElement('button');
-        qaBtn.innerText = 'Q&A';
-
-        const returnBtn = document.createElement('button');
-        returnBtn.innerText = '반품/교환정보';
-
-        const tabBtn = [detailBtn, reviewBtn, qaBtn, returnBtn];
+        const tabBtn = ['상세정보', '리뷰', 'Q&A', '반품/교환정보'];
 
         for(let i = 0; i < tabBtn.length; i++) {
-            tabBtn.setAttribute('class', 'product_tab_btn');
-            productTabWrapper.append(tabBtn);
-
-            tabBtn[i].addEventListener('click', () => {
-                for(let j = 0; j < tabBtn.length; j++) {
-                    tabBtn[j].classList.remove('tab_on');
-                }
-                this.parentNode.classList.add('tab_on');
-            })
+            const li = document.createElement('li');
+            const button = document.createElement('button');
+            button.setAttribute('class', 'detail_tab_btn');
+            button.innerText = tabBtn[i];
+            li.append(button);
+            if(button.innerText === '상세정보') {
+                button.classList.add('tab_on');
+            }
+            detailBtnLi.appendChild(li);
         }
+
+        window.onload = function() {
+            const tabOn = document.querySelectorAll('.detail_tab_btn');
+    
+            for(let j = 0; j < tabOn.length; j++) {
+                tabOn[j].addEventListener('click', () => {
+                    for(let k = 0; k < tabOn.length; k++) {
+                        tabOn[k].classList.remove('tab_on');
+                    }
+                    tabOn[j].classList.add('tab_on');
+                });
+            }
+        }
+
+        productTabWrapper.appendChild(detailBtnLi);
 
         return productTabWrapper;
     }
