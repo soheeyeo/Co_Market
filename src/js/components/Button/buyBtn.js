cae8d05237fe4b00f900b36c6e519e0e194486c0
soheeyeo <sohee407@gmail.com>
2022-11-23 23:18:16 +0900

    Change class name

--- src/js/components/Button/buyBtn.js
+++ src/js/components/Button/buyBtn.js
@@ -4,7 +4,7 @@ export default class BuyBtn extends Component {
 
     render() {
         const buyBtn = document.createElement('button');
-        buyBtn.setAttribute('class', 'buy_button');
+        buyBtn.setAttribute('class', 'buy_btn');
         buyBtn.innerText = '구매하기';
 
         return buyBtn;
