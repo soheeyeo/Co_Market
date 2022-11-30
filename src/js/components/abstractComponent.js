adcc7ecf9992731b0612981f1e2c48bd7456e017
soheeyeo <sohee407@gmail.com>
2022-11-30 12:26:29 +0900

    Modify abstract component

--- src/js/components/abstractComponent.js
+++ src/js/components/abstractComponent.js
@@ -3,7 +3,26 @@ export default class Component {
         this.props = props;
     }
 
+    setState(newState) {
+        this.state = newState;
+        this.updater();
+    }
+
+    updater() {
+        const rendered = this.render();
+        this.lastRendered.replaceWith(rendered);
+        this.lastRendered = rendered;
+    }
+
     render() {
 
+    }
+
+    
+    initialize() {
+        const rendered = this.render();
+        this.lastRendered = rendered;
+
+        return rendered;
     }
 }
