1c5d58122f16ad7e9a11c9e98d4ccf64716a05af
soheeyeo <sohee407@gmail.com>
2022-12-17 20:15:19 +0900

    Add company registration number validation function to checkCrnBtn

--- src/js/components/Button/checkCrnBtn.js
+++ src/js/components/Button/checkCrnBtn.js
@@ -1,6 +1,11 @@
 import Component from "../abstractComponent.js";
+import { checkCrn } from "../../api/api.js";
 
 export default class CheckCrnBtn extends Component {
+    constructor(props) {
+        super(props);
+        this.checkCrn = checkCrn;
+    }
     
     render() {
         const checkCrnBtn = document.createElement('button');
@@ -8,6 +13,12 @@ export default class CheckCrnBtn extends Component {
         checkCrnBtn.classList.add('check_crn_btn');
         checkCrnBtn.type = 'submit';
         checkCrnBtn.innerText = '인증';
+
+        checkCrnBtn.addEventListener('click', async(e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            this.checkCrn();
+        })
 
         return checkCrnBtn;
     }
