cc1c2bf98b579036da41617c55e6b5ed8c95dc82
soheeyeo <sohee407@gmail.com>
2022-12-19 23:09:32 +0900

    Fix enable/disable function of signUpBtn

--- src/js/components/Signup/signUpAgreement.js
+++ src/js/components/Signup/signUpAgreement.js
@@ -1,34 +1,6 @@
 import Component from "../abstractComponent.js";
 
 export default class Agreement extends Component {
-    //버튼 활성화 기능
-    disabledBtn(){
-        const statusMsg = document.querySelector('.status_msg');
-        const checkBtn = document.querySelector('.double_check_icon');
-        const nameInput = document.querySelector('#user_name');
-        const phoneNumStatus = document.querySelector('#user_num + .status_msg')
-        const signUpBtn = document.querySelector('.sign_up_btn');
-        const signUpBuyer = document.querySelector('.sign_up_buyer');
-        const crnStatus = document.querySelector('.seller_input_btn_container + .status_msg');
-        const storeNameInput = document.querySelector('#store_name');
-        if(signUpBuyer.classList.contains('active')) {
-            if(statusMsg.textContent == '사용 가능한 아이디입니다.' && checkBtn.classList.contains('pw_on') && nameInput.value != '' && phoneNumStatus.textContent == '') {
-                signUpBtn.disabled = false;
-                signUpBtn.classList.add('on');
-            } else {
-                signUpBtn.disabled = true;
-                signUpBtn.classList.remove('on');
-            }   
-        } else {
-            if(statusMsg.textContent == '사용 가능한 아이디입니다.' && checkBtn.classList.contains('pw_on') && nameInput.value != '' && phoneNumStatus.textContent == '' && crnStatus.textContent == '사용 가능한 사업자 등록번호입니다.' && storeNameInput.value != '') {
-                signUpBtn.disabled = false;
-                signUpBtn.classList.add('on');
-            } else {
-                signUpBtn.disabled = true;
-                signUpBtn.classList.remove('on');
-            }  
-        }
-        }
     
     render() {
         const formCheckContainer = document.createElement('div');
@@ -43,16 +15,6 @@ export default class Agreement extends Component {
         checkAgree.innerHTML = `
             <a href="" class="agreement txt">이용약관</a> 및 <a href="" class="agreement txt">개인정보처리방침</a>에 대한 내용을 확인하였고 동의합니다.
         `
-
-        checkbox.addEventListener('change', () => {
-            const signUpBtn = document.querySelector('.sign_up_btn');
-            if(checkbox.checked) {
-                this.disabledBtn();
-            } else {
-                signUpBtn.disabled = true;
-                signUpBtn.classList.remove('on');
-            }
-        })
 
         formCheckContainer.append(checkbox, checkAgree);
         
--- src/js/components/Signup/signUpForm.js
+++ src/js/components/Signup/signUpForm.js
@@ -7,10 +7,40 @@ import Agreement from "./signUpAgreement.js";
 import SignUpBtn from "../Button/signUpBtn.js";
 
 export default class SignUpForm extends Component {
+        //버튼 활성화 기능
+        disabledBtn(){
+            const statusMsg = document.querySelector('.status_msg');
+            const checkBtn = document.querySelector('.double_check_icon');
+            const nameInput = document.querySelector('#user_name');
+            const phoneNum = document.querySelector('#user_num');
+            const phoneNumStatus = document.querySelector('#user_num + .status_msg')
+            const signUpBtn = document.querySelector('.sign_up_btn');
+            const signUpBuyer = document.querySelector('.sign_up_buyer');
+            const crnStatus = document.querySelector('.seller_input_btn_container + .status_msg');
+            const storeNameInput = document.querySelector('#store_name');
+            const agreement = document.querySelector('#check_agree');
+            if(signUpBuyer.classList.contains('active')) {
+                if(statusMsg.textContent == '사용 가능한 아이디입니다.' && checkBtn.classList.contains('pw_on') && nameInput.value != '' && phoneNum.value != '' && phoneNumStatus.textContent == '' && agreement.checked) {
+                    signUpBtn.disabled = false;
+                    signUpBtn.classList.add('on');
+                } else {
+                    signUpBtn.disabled = true;
+                    signUpBtn.classList.remove('on');
+                }   
+            } else {
+                if(statusMsg.textContent == '사용 가능한 아이디입니다.' && checkBtn.classList.contains('pw_on') && nameInput.value != '' && phoneNumStatus.textContent == '' && crnStatus.textContent == '사용 가능한 사업자 등록번호입니다.' && storeNameInput.value != '') {
+                    signUpBtn.disabled = false;
+                    signUpBtn.classList.add('on');
+                } else {
+                    signUpBtn.disabled = true;
+                    signUpBtn.classList.remove('on');
+                }  
+            }
+            }
     
     render() {
-        const sighUpForm = document.createElement('form');
-        sighUpForm.setAttribute('class', 'sign_up_form');
+        const signUpForm = document.createElement('form');
+        signUpForm.setAttribute('class', 'sign_up_form');
 
         const signUpType = new SignUpType();
         const signUpInputList = new SignUpInputList();
@@ -19,8 +49,10 @@ export default class SignUpForm extends Component {
         const agreement = new Agreement();
         const signUpBtn = new SignUpBtn();
 
-        sighUpForm.append(signUpType.render(), signUpInputList.initialize(), userInputList.render(), sellerInputList.render(), agreement.render(), signUpBtn.render());
+        setInterval(this.disabledBtn, 1000);
 
-        return sighUpForm;
+        signUpForm.append(signUpType.render(), signUpInputList.initialize(), userInputList.render(), sellerInputList.render(), agreement.render(), signUpBtn.render());
+
+        return signUpForm;
     }
 }
