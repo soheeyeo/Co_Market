b39af7054bbd711c13565004735549124497713a
soheeyeo <sohee407@gmail.com>
2022-12-28 11:41:14 +0900

    Add open modal event to header icon

--- src/js/components/Header/headerIcons.js
+++ src/js/components/Header/headerIcons.js
@@ -1,13 +1,21 @@
 import Component from "../abstractComponent.js";
+import Modal from "../Modal/modal.js";
 
 export default class HeaderIcons extends Component {
+    constructor(props) {
+        super(props);
+        this.modalContent = `로그인이 필요한 서비스입니다.
+        로그인 하시겠습니까?`;
+        this.modalCancelBtn = '아니오';
+        this.modalOkBtn = '예';
+    }
 
     render() {
         const iconsContainer = document.createElement('div');
         iconsContainer.setAttribute('class', 'icons_container');
 
-        const cart = document.createElement('a');
-        cart.setAttribute('href', '/cart');
+        const cart = document.createElement('button');
+        cart.setAttribute('class', 'cart_icon_btn');
         const cartIcon = document.createElement('div');
         cartIcon.setAttribute('class', 'cart_icon');
         const cartIconTxt = document.createElement('span');
@@ -15,6 +23,17 @@ export default class HeaderIcons extends Component {
         cartIconTxt.innerText = '장바구니';
 
         cart.append(cartIcon, cartIconTxt);
+
+        cart.addEventListener('click', (e) => {
+            e.preventDefault();
+            if(localStorage.getItem('token')) {
+                window.location.href = '/cart';
+            } else {
+                const root = document.getElementById('root')
+                const reqModal = new Modal({modalContent:this.modalContent, modalCancelBtn:this.modalCancelBtn, modalOkBtn:this.modalOkBtn, link:'/login'});
+                root.appendChild(reqModal.render());
+            }
+        })
 
         const login = document.createElement('a');
         login.setAttribute('href', '/login');
