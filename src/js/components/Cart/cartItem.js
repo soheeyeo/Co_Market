62e000d40ce5e141f190bba8f6f34559714ff5a9
soheeyeo <sohee407@gmail.com>
2022-12-31 22:42:20 +0900

    Add get cart item detail function

--- src/js/components/Cart/cartItem.js
+++ src/js/components/Cart/cartItem.js
@@ -8,47 +8,48 @@ import { getProductDetail } from "../../api/api.js";
 export default class CartItem extends Component {
     constructor(props) {
         super(props);
+        this.productId = this.props.item.product_id;
+        this.getProductDetail = getProductDetail;
+        this.getProductDetail(this.productId);
         this.state = {
-            product:{
-                price: {}
-            }
+            product:{},
+            isLoaded: false,
         }
     }
-
+    
     render() {
         const cartItemContainer = document.createElement('tr');
         cartItemContainer.setAttribute('class', 'cart_item_container');
-        
-        const td1 = document.createElement('td');
-        const checkbox = document.createElement('input');
-        checkbox.type = 'checkbox';
-        checkbox.id = 'cart_check';
-
-        const label = document.createElement('label');
-        label.htmlFor = 'cart_check';
-        label.innerText = '구매할 상품 선택하기';
-        label.setAttribute('class', 'ir');
-
-        td1.append(checkbox, label);
-
-        const td2 = document.createElement('td');
-
-        const cartItemDetail = new ProductDetailCard({item:this.state.product});
-
-        td2.append(cartItemDetail.render());
-
-        const td3 = document.createElement('td');
-        const cartCountBtn = new CountBtn({stock:this.props.item.stock, price:this.props.item.price});
-        td3.appendChild(cartCountBtn.render());
-
-        const td4 = document.createElement('td');
-        const cartTotalPriice = new ProductTotal({price:this.props.item.price});
-
-        const buyBtn = new BuyBtn();
-
-        td4.append(cartTotalPriice, buyBtn.render());
-
-        cartItemContainer.append(td1, td2, td3, td4);
+        if(this.state.isLoaded) {
+            const td1 = document.createElement('td');
+            const checkbox = document.createElement('input');
+            checkbox.type = 'checkbox';
+            checkbox.id = 'cart_check';
+    
+            const label = document.createElement('label');
+            label.htmlFor = 'cart_check';
+            label.innerText = '구매할 상품 선택하기';
+            label.setAttribute('class', 'ir');
+    
+            td1.append(checkbox, label);
+    
+            const td2 = document.createElement('td');
+            const td3 = document.createElement('td');
+            const td4 = document.createElement('td');
+    
+            const cartItemDetail = new ProductDetailCard({item:this.state.product});
+    
+            td2.append(cartItemDetail.render());
+    
+            const cartCountBtn = new CountBtn({qty:this.props.item.quantity});
+            td3.appendChild(cartCountBtn.render());
+    
+            const cartTotalPriice = new ProductTotal({price:this.state.product.price, qty:this.props.item.quantity});
+    
+            const buyBtn = new BuyBtn();
+            td4.append(cartTotalPriice.render(), buyBtn.render());
+            cartItemContainer.append(td1, td2, td3, td4);
+        }
 
         return cartItemContainer;
     }
