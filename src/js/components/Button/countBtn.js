a2f41d437799cbbde36b8c893d1e0cf78aa5d7e2
soheeyeo <sohee407@gmail.com>
2023-01-19 14:19:13 +0900

    Delete conditional statements that apply to Cart View

--- src/js/components/Button/countBtn.js
+++ src/js/components/Button/countBtn.js
@@ -5,48 +5,24 @@ export default class CountBtn extends Component {
     render() {
         const countBtn = document.createElement('div');
         countBtn.setAttribute('class', 'count_btn');
-        if(window.location.pathname.includes('/product')) {
-            const mBtn = document.createElement('input');
-            mBtn.setAttribute('class', 'm_btn');
-            mBtn.type = 'button';
-            mBtn.value = '-';
-            mBtn.addEventListener('click', this.props.decreaseQty);
 
-            const count = document.createElement('span');
-            count.setAttribute('class', 'count');
-            count.innerText = this.props.qty;
-    
-            const pBtn = document.createElement('input');
-            pBtn.setAttribute('class', 'p_btn');
-            pBtn.type = 'button';
-            pBtn.value = '+';
-            pBtn.addEventListener('click', this.props.increaseQty);
+        const mBtn = document.createElement('input');
+        mBtn.setAttribute('class', 'm_btn');
+        mBtn.type = 'button';
+        mBtn.value = '-';
+        mBtn.addEventListener('click', this.props.decreaseQty);
 
-            countBtn.append(mBtn, count, pBtn);
-        } 
-        
-        else if(window.location.pathname === '/cart') {
-            countBtn.classList.add('cart_count');
-            const mBtn = document.createElement('input');
-            mBtn.setAttribute('class', 'm_btn');
-            mBtn.type = 'button';
-            mBtn.value = '-';
-            mBtn.addEventListener('click', this.props.decreaseQty);
+        const count = document.createElement('span');
+        count.setAttribute('class', 'count');
+        count.innerText = this.props.qty;
 
-            const count = document.createElement('input');
-            count.setAttribute('class', 'count_value');
-            count.type = 'number';
-            count.value = this.props.qty;
-            count.disabled = true;
-    
-            const pBtn = document.createElement('input');
-            pBtn.setAttribute('class', 'p_btn');
-            pBtn.type = 'button';
-            pBtn.value = '+';
-            pBtn.addEventListener('click', this.props.increaseQty);
+        const pBtn = document.createElement('input');
+        pBtn.setAttribute('class', 'p_btn');
+        pBtn.type = 'button';
+        pBtn.value = '+';
+        pBtn.addEventListener('click', this.props.increaseQty);
 
-            countBtn.append(mBtn, count, pBtn);
-        }
+        countBtn.append(mBtn, count, pBtn);
 
         return countBtn;
     }
