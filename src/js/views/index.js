64464fb85e2873f2b097c59b37e40f4f77ea68c3
soheeyeo <sohee407@gmail.com>
2023-01-27 20:42:46 +0900

    Rename component to lowercase

--- src/js/views/index.js
+++ src/js/views/index.js
@@ -1,5 +1,6 @@
 export {default as Main} from './main.js';
 export {default as Login} from './login.js';
-export {default as SignUp} from './signUp.js';
+export {default as SignUp} from './signup.js';
 export {default as ProductDetail} from './productDetail.js';
 export {default as Cart} from './cart.js';
+export {default as Order} from './order.js';
