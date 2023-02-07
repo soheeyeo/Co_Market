65dcc720ea3aa688a73a35f291c7a378be9e350d
soheeyeo <sohee407@gmail.com>
2023-02-07 22:17:58 +0900

    Add components to Order component

--- src/js/views/order.js
+++ src/js/views/order.js
@@ -1,5 +1,9 @@
 import Component from "../components/abstractComponent.js";
 import Header from "../components/Header/header.js";
+import OrderList from "../components/Order/orderList.js";
+import OrderForm from "../components/Order/orderForm.js";
+import OrderMethod from "../components/Order/orderMethod.js";
+import OrderTotal from "../components/Order/orderTotal.js";
 
 export default class Order extends Component{
 
@@ -14,7 +18,16 @@ export default class Order extends Component{
         orderTit.setAttribute('class', 'order_tit');
         orderTit.innerText = '주문/결제하기';
 
-        orderContainer.append(orderTit);
+        const orderList = new OrderList();
+        const orderForm = new OrderForm();
+
+        const paymentContainer = document.createElement('div');
+        paymentContainer.setAttribute('class', 'total_payment_container');
+        const orderMethod = new OrderMethod();
+        const orderTotal = new OrderTotal();
+        paymentContainer.append(orderMethod.render(), orderTotal.render());
+
+        orderContainer.append(orderTit, orderList.render(), orderForm.render(), paymentContainer);
 
         frag.append(header.render(), orderContainer);
 
