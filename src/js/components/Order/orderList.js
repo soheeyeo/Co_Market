8b6722df3f4fc0c1a4a2e09903a0573fd9d4ab78
soheeyeo <sohee407@gmail.com>
2023-02-08 19:11:55 +0900

    Add OrderListItem component to OrderList component

--- src/js/components/Order/orderList.js
+++ src/js/components/Order/orderList.js
@@ -1,11 +1,21 @@
 import Component from "../abstractComponent.js";
+import OrderListItem from "./orderListItem.js";
 
 export default class OrderList extends Component {
     constructor(props) {
         super(props);
+        this.item = JSON.parse(sessionStorage.getItem('cart'));
+        this.total = 0;
+    }
+
+    orderTotal() {
+        this.item.forEach((item) => {
+            this.total += item.price * item.qty;
+        });
     }
 
     render() {
+        this.orderTotal();
         const orderList = document.createElement('table');
         orderList.setAttribute('class', 'order_list');
 
@@ -25,9 +35,24 @@ export default class OrderList extends Component {
         orderListTit.appendChild(tr);
 
         const tbody = document.createElement('tbody');
-        console.log(this.props)
+        this.item.map((item) => {
+            const orderListItem = new OrderListItem({item:item})
+            tbody.append(orderListItem.render());
+        })
 
-        orderList.append(orderListTit, tbody);
+        const tfoot = document.createElement('tfoot');
+        tfoot.setAttribute('class', 'order_item_total_price');
+        const orderTotalPriceTxt = document.createElement('span');
+        orderTotalPriceTxt.setAttribute('class', 'order_total_price_txt');
+        orderTotalPriceTxt.innerText = '총 주문금액';
+
+        const orderTotalPrice = document.createElement('strong');
+        orderTotalPrice.setAttribute('class', 'order_total_price');
+        orderTotalPrice.innerText = `${(this.total).toLocaleString('ko-KR')+'원'}`;
+
+        tfoot.append(orderTotalPriceTxt, orderTotalPrice);
+
+        orderList.append(orderListTit, tbody, tfoot);
 
     return orderList;
     }
