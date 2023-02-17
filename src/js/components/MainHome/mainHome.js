f234ce62001de92bbccb992a91e8319b3c2c0c72
soheeyeo <sohee407@gmail.com>
2023-02-17 16:09:56 +0900

    Add prev and next button for pagination

--- src/js/components/MainHome/mainHome.js
+++ src/js/components/MainHome/mainHome.js
@@ -6,15 +6,28 @@ export default class MainHome extends Component {
     constructor(props) {
         super(props);
         this.getProductData = getProductData;
-        this.getProductData(1);
         this.state = {
             product:[],
+            currentPage:1,
             isLoaded:false
         };
+        this.firstPage = this.state.currentPage - (this.state.currentPage % 5) + 1;
+        this.lastPage = this.state.currentPage - (this.state.currentPage % 5) + 5;
+        this.getProductData(this.state.currentPage);
     }
 
     setPage(i) {
         this.setState({...this.state, currentPage:i+1});
+        this.getProductData(this.state.currentPage);
+    }
+
+    prevPage() {
+        this.setState({...this.state, currentPage:this.state.currentPage - 1});
+        this.getProductData(this.state.currentPage);
+    }
+
+    nextPage() {
+        this.setState({...this.state, currentPage:this.state.currentPage + 1});
         this.getProductData(this.state.currentPage);
     }
 
@@ -27,7 +40,20 @@ export default class MainHome extends Component {
     
             const paginationUl = document.createElement('ul');
             paginationUl.setAttribute('class', 'pagination_ul');
-    
+            
+            const prevBtn = document.createElement('button');
+            prevBtn.setAttribute('class', 'pagination_prev_btn');
+            if(this.state.currentPage === 1) {
+                prevBtn.disabled = true;
+                prevBtn.classList.add('left_disabled');
+            }
+            prevBtn.addEventListener('click', (e) => {
+                e.preventDefault();
+                e.stopPropagation();
+                this.prevPage();
+            })
+            paginationUl.append(prevBtn);
+            
             for(let i = 0; i < this.state.pageNum; i++) {
                 const paginationLi = document.createElement('li');
                 const paginationBtn = document.createElement('button');
@@ -37,13 +63,26 @@ export default class MainHome extends Component {
                     e.stopPropagation();
                     this.setPage(i);
                 })
-                if(this.state.currentPage == i+1) {
+                if(this.state.currentPage === i+1) {
                     paginationBtn.classList.add('btn_on');
                 };
                 paginationLi.append(paginationBtn);
                 paginationUl.append(paginationLi);
             }
-    
+
+            const nextBtn = document.createElement('button');
+            nextBtn.setAttribute('class', 'pagination_next_btn');
+            if(this.state.currentPage === this.lastPage) {
+                nextBtn.disabled = true;
+                nextBtn.classList.add('right_disabled');
+            }
+            nextBtn.addEventListener('click', (e) => {
+                e.preventDefault();
+                e.stopPropagation();
+                this.nextPage();
+            })
+            paginationUl.append(nextBtn);
+            
             paginationContainer.append(paginationUl);
 
             mainElement.append(productList.initialize(), paginationContainer);
