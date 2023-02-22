51c659488baf54b0d1bd9c4a958096099ba1367a
soheeyeo <sohee407@gmail.com>
2023-02-22 19:11:59 +0900

    Add order function

--- src/js/components/Button/buyBtn.js
+++ src/js/components/Button/buyBtn.js
@@ -1,5 +1,6 @@
 import Component from "../abstractComponent.js";
 import Modal from "../Modal/modal.js";
+import { order } from "../../api/api.js";
 
 export default class BuyBtn extends Component {
     constructor(props) {
@@ -14,10 +15,12 @@ export default class BuyBtn extends Component {
     
     detailSaveItem() {
         sessionStorage.setItem('cart', JSON.stringify(this.item));
+        sessionStorage.setItem('order_kind', 'direct_order');
     }
 
     cartSaveItem() {
         sessionStorage.setItem('cart', JSON.stringify(this.item));
+        sessionStorage.setItem('order_kind', 'cart_one_order');
     }
 
     render() {
@@ -49,6 +52,47 @@ export default class BuyBtn extends Component {
 }
 
 export class OrderBtn extends Component {
+    constructor(props) {
+        super(props);
+        this.item = JSON.parse(sessionStorage.getItem('cart'));
+        this.orderKind = sessionStorage.getItem('order_kind');
+        this.total = 0;
+        this.shipping = 0;
+        if(sessionStorage.getItem('order_kind') === 'cart_order') {
+            this.product_id = '';
+            this.qty = '';
+            this.item.forEach((item) => {
+                this.total += item.price * item.qty;
+                this.shipping += item.shipping_fee;
+            });
+            this.totalPrice = this.total + this.shipping;
+        } else {
+            this.product_id = this.item.product_id;
+            this.qty = this.item.qty;
+            this.totalPrice = this.item.price * this.item.qty + this.item.shipping_fee;
+        }
+    }
+
+    async reqOrder() {
+        const reqData = {
+            product_id: this.product_id,
+            quantity: this.qty,
+            order_kind: this.orderKind,
+            receiver: document.querySelector('#recipient_name').value,
+            receiver_phone_number: document.querySelector('#recipient_tel').value,
+            address: document.querySelector('#recipient_address').value + document.querySelector('#recipient_address_detail').value,
+            address_message: document.querySelector('#message').value,
+            payment_method: document.querySelector('.payment_fieldset input[name=order_method]:checked').value,
+            total_price: this.totalPrice,
+        };
+        const res = await order(reqData);
+        console.log(res);
+    }
+
+    cartSaveItem() {
+        sessionStorage.setItem('cart', JSON.stringify(this.item));
+        sessionStorage.setItem('order_kind', 'cart_order');
+    }
 
     render() {
         const buyBtn = document.createElement('button');
@@ -63,6 +107,11 @@ export class OrderBtn extends Component {
             buyBtn.setAttribute('class', 'big_order_btn');
             buyBtn.innerText = '결제하기';
             buyBtn.disabled = true;
+            buyBtn.addEventListener('click', (e) => {
+                e.preventDefault();
+                e.stopPropagation();
+                this.reqOrder();
+            })
         }
 
         return buyBtn;
