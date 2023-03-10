df99c608ec09f1e1fcabc107480e9736f689e12d
soheeyeo <sohee407@gmail.com>
2023-03-10 22:31:02 +0900

    Create Editor component

---
+++ src/js/components/ProductUpload/editor.js
@@ -0,0 +1,18 @@
+import Component from "../abstractComponent.js";
+import UploadNotice from "./uploadNotice.js";
+import UploadForm from "./uploadForm.js";
+
+export default class Editor extends Component {
+
+    render() {
+        const main = document.createElement('main');
+        
+        const uploadNotice = new UploadNotice();
+
+        const uploadForm = new UploadForm();
+
+        main.append(uploadNotice.render(), uploadForm.render());
+
+        return main;
+    }
+}
