bef22fe7a6fdb8caa8736c424ed3d9a5728364cc
soheeyeo <sohee407@gmail.com>
2023-03-10 22:30:40 +0900

    Create UploadForm component

---
+++ src/js/components/ProductUpload/uploadForm.js
@@ -0,0 +1,41 @@
+import Component from "../abstractComponent.js";
+import UploadImg from "./uploadImg.js";
+import UploadInfo from "./uploadInfo.js";
+import UploadDetail from "./uploadDetail.js";
+
+export default class UploadForm extends Component {
+
+    render() {
+        const uploadForm = document.createElement('form');
+        uploadForm.method = 'post';
+
+        const productInfoContainer = document.createElement('div');
+        productInfoContainer.setAttribute('class', 'product_info_upload_container');
+
+        const uploadImg = new UploadImg();
+
+        const uploadInfo = new UploadInfo();
+
+        productInfoContainer.append(uploadImg.render(), uploadInfo.render());
+
+        const uploadDetail = new UploadDetail();
+
+        const btnContainer = document.createElement('div');
+        btnContainer.setAttribute('class', 'upload_btn_container');
+
+        const cancelBtn = document.createElement('button');
+        cancelBtn.setAttribute('class', 'upload_cancel_btn');
+        cancelBtn.innerText = '취소';
+
+        const saveBtn = document.createElement('button');
+        saveBtn.setAttribute('class', 'upload_save_btn');
+        saveBtn.innerText = '저장하기';
+        saveBtn.type = 'submit';
+
+        btnContainer.append(cancelBtn, saveBtn);
+
+        uploadForm.append(productInfoContainer, uploadDetail.render(), btnContainer);
+
+        return uploadForm;
+    }
+}
