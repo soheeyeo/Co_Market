ad3962edb6b30a93d7383c50ed973788845cfcf4
soheeyeo <sohee407@gmail.com>
2023-03-16 23:50:41 +0900

    Move handleChangeImg from UploadImg to UploadForm

--- src/js/components/ProductUpload/uploadForm.js
+++ src/js/components/ProductUpload/uploadForm.js
@@ -2,8 +2,43 @@ import Component from "../abstractComponent.js";
 import UploadImg from "./uploadImg.js";
 import UploadInfo from "./uploadInfo.js";
 import UploadDetail from "./uploadDetail.js";
+import SaveUploadBtn from "../Button/saveUploadBtn.js";
+import { uploadProduct } from "../../api/api.js";
 
 export default class UploadForm extends Component {
+    constructor(props) {
+        super(props);
+        this.uploadProduct = uploadProduct;
+        this.modalContent1 = '올바른 형식의 이미지 파일을 선택해주세요.';
+        this.modalContent2 = '이미지는 1개만 선택이 가능합니다.';
+        this.modalOkBtn = '확인';
+        this.item = JSON.parse(sessionStorage.getItem('item'));
+        this.state = {
+            img: '',
+            url: ''
+        }
+    }
+
+    handleChangeImg() {
+        const input = document.querySelector('#img_upload');
+        const file = input.files[0];
+        
+        if(!file.type.match('image/.*')) {
+            const root = document.getElementById('root');
+            const reqModal = new Modal({modalContent:this.modalContent1, modalOkBtn:this.modalOkBtn});
+            root.appendChild(reqModal.initialize());   
+        } else if(file.length > 1) {
+            const root = document.getElementById('root');
+            const reqModal = new Modal({modalContent:this.modalContent2, modalOkBtn:this.modalOkBtn});
+            root.appendChild(reqModal.initialize());
+        }
+
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => {
+            this.setState({url:file, img:reader.result});
+        }
+    }
 
     render() {
         const uploadForm = document.createElement('form');
@@ -12,13 +47,13 @@ export default class UploadForm extends Component {
         const productInfoContainer = document.createElement('div');
         productInfoContainer.setAttribute('class', 'product_info_upload_container');
 
-        const uploadImg = new UploadImg();
+        const uploadImg = new UploadImg({img:this.state.img, handleChangeImg:this.handleChangeImg.bind(this), bgImg: this.item ? this.item.image : ''});
 
-        const uploadInfo = new UploadInfo();
+        const uploadInfo = new UploadInfo({item:this.item ? this.item : ''});
 
         productInfoContainer.append(uploadImg.initialize(), uploadInfo.render());
 
-        const uploadDetail = new UploadDetail();
+        const uploadDetail = new UploadDetail({detail:this.item ? this.item.product_info : ''});
 
         const btnContainer = document.createElement('div');
         btnContainer.setAttribute('class', 'upload_btn_container');
@@ -28,12 +63,9 @@ export default class UploadForm extends Component {
         cancelBtn.innerText = '취소';
         cancelBtn.type = 'button';
 
-        const saveBtn = document.createElement('button');
-        saveBtn.setAttribute('class', 'upload_save_btn');
-        saveBtn.innerText = '저장하기';
-        saveBtn.type = 'submit';
+        const saveBtn = new SaveUploadBtn({image: this.item ? this.item.image : this.state.url, item:this.item});
 
-        btnContainer.append(cancelBtn, saveBtn);
+        btnContainer.append(cancelBtn, saveBtn.render());
 
         uploadForm.append(productInfoContainer, uploadDetail.render(), btnContainer);
 
