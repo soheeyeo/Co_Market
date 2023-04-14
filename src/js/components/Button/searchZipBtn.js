8f158ff0f50fe1181ba0fe7bb346b8d919a2839e
soheeyeo <sohee407@gmail.com>
2023-04-15 00:24:15 +0900

    Delete comment

--- src/js/components/Button/searchZipBtn.js
+++ src/js/components/Button/searchZipBtn.js
@@ -5,24 +5,6 @@ export default class SearchZipBtn extends Component {
         super(props);
     }
     
-    // sample6_execDaumPostcode() {
-    //     new daum.Postcode({
-    //         oncomplete: function(data) {
-    //             var addr = '';
-
-    //             if (data.userSelectedType === 'R') {
-    //                 addr = data.roadAddress;
-    //             } else {
-    //                 addr = data.jibunAddress;
-    //             }
-
-    //             document.getElementById('recipient_address_zip_code').value = data.zonecode;
-    //             document.getElementById("recipient_address").value = addr;
-    //             document.getElementById("recipient_address_detail").focus();
-    //         }
-    //     }).open();
-    // }
-    
     render() {
         const searchZipBtn = document.createElement('button');
         searchZipBtn.setAttribute('class', 'search_zip_btn');
