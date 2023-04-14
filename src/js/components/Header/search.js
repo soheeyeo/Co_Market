eadaec1952724612ba0584a13ef65c3a9fa38e93
soheeyeo <sohee407@gmail.com>
2023-04-15 00:21:49 +0900

    Add searchProduct function to Search component

--- src/js/components/Header/search.js
+++ src/js/components/Header/search.js
@@ -1,6 +1,17 @@
 import Component from "../abstractComponent.js";
+import { searchProduct } from "../../api/api.js";
 
 export default class Search extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    async searchProduct(keyword) {
+        const data = await searchProduct(keyword);
+        sessionStorage.setItem('searchData', JSON.stringify(data.results));
+        sessionStorage.setItem('keyword', keyword);
+        window.routing('/search');
+    }
 
     render() {
         const searchBox = document.createElement('div');
@@ -16,12 +27,26 @@ export default class Search extends Component {
         search.setAttribute('class', 'search');
         search.name = 'search';
         search.type = 'text';
-        search.placeholder = '상품을 검색해보세요!'
+        search.placeholder = '상품을 검색해보세요!';
+
+        search.addEventListener('keydown', (e) => {
+            if(search.value !== ''&& e.key === 'Enter') {
+                this.searchProduct(search.value);
+            }
+        })
 
         const searchIcon = document.createElement('button');
         searchIcon.setAttribute('class', 'search_icon');
         searchIcon.type = 'submit';
         searchIcon.name = 'search_btn';
+
+        searchIcon.addEventListener('click', (e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            if(search.value !== '') {
+                this.searchProduct(search.value);
+            }
+        })
 
         searchForm.append(search, searchIcon);
 
